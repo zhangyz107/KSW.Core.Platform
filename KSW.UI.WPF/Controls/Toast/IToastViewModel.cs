4cb03d18de9f02d6030119cd7ba5b36f229efeda
zhangyz107 <zhangyz10715@163.com>
2025-10-10 16:01:19 +0800

    新增Toast视图模型接口

---
+++ KSW.UI.WPF/Controls/Toast/IToastViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSW.UI.WPF.Controls
+{
+    /// <summary>
+    /// Toast视图模型接口
+    /// </summary>
+    public interface IToastViewModel
+    {
+        WindowToastManager? ToastManager { get; set; }
+    }
+}
